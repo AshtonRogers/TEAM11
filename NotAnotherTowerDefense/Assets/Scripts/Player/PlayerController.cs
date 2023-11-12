745f45ef233a9568dfab741ce884f4ef905785f1
Patrick Gleeson <130936504+WarTurtleMedia@users.noreply.github.com>
2023-11-13 00:13:08 +1300

    + Start of Placement for Towers

--- NotAnotherTowerDefense/Assets/Scripts/Player/PlayerController.cs
+++ NotAnotherTowerDefense/Assets/Scripts/Player/PlayerController.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+enum PlacementMode
+{
+    Mode_Range,
+    Mode_Melee,
+    Mode_Inactive
+}
+
 public class PlayerController : MonoBehaviour
 {
     public TownScript town;
+
+    PlacementMode m_CurrentMode = PlacementMode.Mode_Inactive;
+
+    [SerializeField] private GameObject m_RangeTowerRef;
+    [SerializeField] private GameObject m_MeleeTowerRef;
+
+    private bool m_IsPlacing = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +30,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PlaceTower(PlacementMode.Mode_Melee);
+        }
+
+
         //update currencys
         town.ClaimTownSilver();
 
@@ -59,5 +80,59 @@ public class PlayerController : MonoBehaviour
                 town.attackTowerRanges[i].PayCost();
             }
         }
+    }
+
+    void PlaceTower(PlacementMode _Mode)
+    {
+        m_CurrentMode = _Mode;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        switch (m_CurrentMode)
+        {
+            case PlacementMode.Mode_Melee:
+                GameObject newMelee = Instantiate(m_MeleeTowerRef, mousePosition, Quaternion.identity);
+
+                if (CheckPlacement(newMelee) == false)
+                {
+                    Destroy(newMelee);
+                }
+                else
+                {
+
+
+                }
+                break;
+            case PlacementMode.Mode_Range:
+
+                break;
+        }
+    }
+
+    bool CheckPlacement(GameObject _TowerObj)
+    {
+        
+        GameObject[] Path = GameObject.FindGameObjectsWithTag("Path");
+        GameObject[] Towers = GameObject.FindGameObjectsWithTag("Towers");
+
+        foreach (GameObject _Tower in Towers)
+        {
+            if (_TowerObj.GetComponent<BoxCollider2D>().bounds.Intersects(_Tower.GetComponent<BoxCollider2D>().bounds) && _TowerObj != _Tower)
+            {
+                return false;
+            }
+
+        }
+
+        foreach (GameObject _Path in Path)
+        {
+            if (_TowerObj.GetComponent<BoxCollider2D>().bounds.Intersects(_Path.GetComponent<BoxCollider2D>().bounds))
+            {
+                return false;
+            }
+
+        }
+
+
+        return true;
     }
 }
